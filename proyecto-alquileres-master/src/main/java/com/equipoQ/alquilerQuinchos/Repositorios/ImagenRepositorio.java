22ad073e0698b6f6a4aeb5ae716310e0b396686d
Marcelo <131071603+Mrcx3@users.noreply.github.com>
2023-11-21 22:05:20 -0300

    Delete proyecto-alquileres-master directory

--- proyecto-alquileres-master/src/main/java/com/equipoQ/alquilerQuinchos/Repositorios/ImagenRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.equipoQ.alquilerQuinchos.Repositorios;
-import com.equipoQ.alquilerQuinchos.Entidades.*;
-import org.springframework.data.jpa.repository.*;
-import org.springframework.stereotype.*;
-@Repository
-public interface ImagenRepositorio extends JpaRepository<Imagen,String> {
-   
-}
